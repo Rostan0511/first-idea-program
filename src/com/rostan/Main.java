a26c0b644daadafd7442f575a57b49602ecf6bad
Rostan0511 <loborostan05@gmail.com>
2025-06-22 00:38:12 +0530

    basics

--- src/com/rostan/Main.java
+++ src/com/rostan/Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World\nMy name is Rostan Lobo");
-        Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+//        System.out.println("Hello World\nMy name is Rostan Lobo");
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(input.nextInt());
+        System.out.println("Γειά σου");
     }
 }
