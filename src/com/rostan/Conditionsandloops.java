17b0e84e81aa9a683b0919c1aa0c014a78868fc7
Rostan0511 <loborostan05@gmail.com>
2025-06-25 13:50:14 +0530

    alpahbetcheck

--- src/com/rostan/Conditionsandloops.java
+++ src/com/rostan/Conditionsandloops.java
@@ -4,16 +4,22 @@ import java.util.Scanner;
 
 public class Conditionsandloops {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int salary = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        int salary = sc.nextInt();
+//
+//        if (salary > 10000 && salary < 20000){
+//            salary += 2000;
+//        } else if (salary>20000) {
+//            salary+=4000;
+//        } else {
+//            salary += 1000;
+//        }
+//        System.out.println(salary);
 
-        if (salary > 10000 && salary < 20000){
-            salary += 2000;
-        } else if (salary>20000) {
-            salary+=4000;
-        } else {
-            salary += 1000;
-        }
-        System.out.println(salary);
+        int a = 120;
+        int b = 22;
+        int c = 12;
+        int Max = Math.max(c,Math.max(a,b));
+        System.out.println(Max);
     }
 }
